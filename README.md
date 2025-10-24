419a974f653b20ff4f8e2c7f3c408c581a262edf
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2025-10-24 16:57:42 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Beta 1.3_01
+<img width="854" height="480" alt="image" src="https://raw.githubusercontent.com/EaglerPorts/b1.3_01z/refs/heads/main/img/mainmenu.png" />
---
+++ src/game/java/net/minecraft/src/SoundManager.java
@@ -0,0 +1,53 @@
+package net.minecraft.src;
+
+import net.lax1dude.eaglercraft.internal.vfs2.VFile2;
+
+public class SoundManager {
+	public void loadSoundSettings(GameSettings var1) {
+
+	}
+
+	private void tryToSetLibraryAndCodecs() {
+
+	}
+
+	public void onSoundOptionsChanged() {
+
+	}
+
+	public void closeMinecraft() {
+
+	}
+
+	public void addSound(String var1, VFile2 var2) {
+
+	}
+
+	public void addStreaming(String var1, VFile2 var2) {
+
+	}
+
+	public void addMusic(String var1, VFile2 var2) {
+
+	}
+
+	public void playRandomMusicIfReady() {
+
+	}
+
+	public void func_338_a(EntityLiving var1, float var2) {
+
+	}
+
+	public void func_331_a(String var1, float var2, float var3, float var4, float var5, float var6) {
+
+	}
+
+	public void playSound(String var1, float var2, float var3, float var4, float var5, float var6) {
+
+	}
+
+	public void func_337_a(String var1, float var2, float var3) {
+
+	}
+}
