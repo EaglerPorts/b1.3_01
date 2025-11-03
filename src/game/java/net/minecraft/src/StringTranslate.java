be96bf08a0176ee4264f0f3ddc836926bc9b4219
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2025-11-03 12:59:39 -0600

    init

---
+++ src/game/java/net/minecraft/src/StringTranslate.java
@@ -0,0 +1,42 @@
+package net.minecraft.src;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import net.lax1dude.eaglercraft.EagRuntime;
+import net.lax1dude.eaglercraft.HString;
+
+public class StringTranslate {
+	private static StringTranslate instance = new StringTranslate();
+	private Properties translateTable = new Properties();
+
+	private StringTranslate() {
+		try {
+			this.translateTable.load(EagRuntime.getResourceStream("/lang/en_US.lang"));
+		} catch (IOException var2) {
+			var2.printStackTrace();
+		}
+
+	}
+
+	public static StringTranslate getInstance() {
+		return instance;
+	}
+
+	public String translateKey(String var1) {
+		return this.translateTable.getProperty(var1, var1);
+	}
+
+	public String translateKeyFormat(String var1, Object... var2) {
+		String var3 = this.translateTable.getProperty(var1, var1);
+		return HString.format(var3, var2);
+	}
+
+	public String translateNamedKey(String var1) {
+		return this.translateTable.getProperty(var1 + ".name", "");
+	}
+
+	public boolean hasTranslateKey(String var0) {
+		return this.translateTable.containsKey(var0);
+	}
+}
